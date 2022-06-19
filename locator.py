9d3da2101aeebab44863a4e1c15cc1546de0f9bb
takrim1999 <takrimulalom@gmail.com>
2022-06-19 21:36:46 +0600

    done for white backgroud and ground objects

--- locator.py
+++ locator.py
@@ -2,7 +2,7 @@ import pyautogui
 n = 0
 while n<100:
     try:
-        print(pyautogui.locateOnScreen('clean.png'))
+        print(pyautogui.locateOnScreen('new.png'))
     except:
         pass
     n = n + 1
